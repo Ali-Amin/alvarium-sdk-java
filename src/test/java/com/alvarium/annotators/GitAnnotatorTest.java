dac814cc0b687a8d48f61a47f4c185448d6a04d3
Karim Elghamry <karimelghamry@gmail.com>
2023-03-28 12:49:47 +0200

    Add GitAnnotator implementation and unit tests
    * add: GitAnnotator implementation
    * add: Git AnnotationType
    * add: GitAnnotator to AnnotatorFactory
    * add: relevant unit tests
    
    Co-authored-by: Ali Amin <ali.m.amin98@gmail.com>
    Signed-off-by: Ali Amin <ali.m.amin98@gmail.com>

---
+++ src/test/java/com/alvarium/annotators/GitAnnotatorTest.java
@@ -0,0 +1,54 @@
+package com.alvarium.annotators;
+
+import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.junit.Test;
+
+import com.alvarium.SdkInfo;
+import com.alvarium.contracts.Annotation;
+import com.alvarium.contracts.AnnotationType;
+import com.alvarium.hash.HashInfo;
+import com.alvarium.hash.HashType;
+import com.alvarium.sign.KeyInfo;
+import com.alvarium.sign.SignType;
+import com.alvarium.sign.SignatureInfo;
+import com.alvarium.utils.ImmutablePropertyBag;
+import com.alvarium.utils.PropertyBag;
+
+public class GitAnnotatorTest {
+
+        // test commented out as it is considered an integration test (depends on git)
+        // @Test
+        public void executeShouldCreateAnnotation() throws AnnotatorException {
+                AnnotatorFactory factory = new AnnotatorFactory();
+                KeyInfo privateKey = new KeyInfo(
+                                "./src/test/java/com/alvarium/annotators/public.key",
+                                SignType.Ed25519);
+                KeyInfo publicKey = new KeyInfo(
+                                "./src/test/java/com/alvarium/annotators/public.key",
+                                SignType.Ed25519);
+
+                SignatureInfo sign = new SignatureInfo(publicKey, privateKey);
+                final AnnotationType[] annotators = { AnnotationType.GIT };
+                final SdkInfo config = new SdkInfo(annotators, new HashInfo(HashType.MD5Hash), sign, null);
+                Annotator tpm = factory.getAnnotator(AnnotationType.GIT, config);
+
+                final Map<String, Object> annotatorProperties = new HashMap<>();
+
+                annotatorProperties.put(
+                        "directory", 
+                        new File(System.getProperty("user.dir"))
+                );
+                annotatorProperties.put(
+                        "commitHash", 
+                        "04bae059747f700858ef46c28abbd7fa6efc7036"
+                );
+
+                PropertyBag ctx = new ImmutablePropertyBag(Map.of(AnnotationType.GIT.name(), annotatorProperties));
+                byte[] data = "pipeline1/1".getBytes();
+                Annotation annotation = tpm.execute(ctx, data);
+                System.out.println(annotation.toJson());
+        }
+}
